e6de593a0f714859f4284b4921cc7965da217b8d
Nabin Khanal <48934280+nabinkhanal128@users.noreply.github.com>
2024-08-10 01:11:20 +0545

    Final Commit

--- Scrapper Indeed.py
+++ Scrapper Indeed.py
@@ -10,17 +10,19 @@ driver = webdriver.Chrome()
 driver.get('https://www.indeed.com/companies/best-companies')
 driver.maximize_window()
 
-# Create a dictionary to store the scraped data
+# Creating a dictionary to store the scraped data
 data = {
     'Company Name': [],
     'Reviews': [],
     'Total Reviews': [],
-    'Company Type/Employees Estimation': [],
+    'Company Type': [],
+    'Employees Estimation': [],
     'Company Description': []
 }
 
 try:
-    for i in range(1, 10):
+    for i in range(1, 150):
+        # Wait until the data is loaded
         WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, 'h2')))
         
         # Scrape data
@@ -30,19 +32,30 @@ try:
         company_types = driver.find_elements(By.CLASS_NAME, 'css-j3kgaw.e1wnkr790')
         company_descriptions = driver.find_elements(By.CLASS_NAME, 'css-1io73yq.eu4oa1w0')
 
-        # Store the data in the dictionary
+        # Storeing the data in the dictionary
         for name, review, total_review, company_type, description in zip(company_names, reviews, total_reviews, company_types, company_descriptions):
             data['Company Name'].append(name.text)
             data['Reviews'].append(review.text)
             data['Total Reviews'].append(total_review.text)
-            data['Company Type/Employees Estimation'].append(company_type.text)
+            
+            # Split company_type
+            split_types = company_type.text.split(' - ')
+            if len(split_types) > 1:
+                data['Company Type'].append(split_types[0])
+                data['Employees Estimation'].append(split_types[1])
+            else:
+                data['Company Type'].append(split_types[0])
+                data['Employees Estimation'].append('N/A')
+            
             data['Company Description'].append(description.text)
 
         # Find and click the button with a specific title attribute
         try:
-            button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '[title="Next"]')))
+            button = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '[title="Next"]'))
+            )
             driver.execute_script("arguments[0].scrollIntoView(true);", button)
-            button.click()
+            driver.execute_script("arguments[0].click();", button)
             time.sleep(5)
         except Exception as e:
             print(f"Button click failed: {e}")
@@ -51,8 +64,8 @@ try:
 finally:
     driver.quit()
 
-# Write the dictionary data to a CSV file
-csv_file = 'c.csv'
+# Writing the dictionary data to a CSV file
+csv_file = 'companies_data.csv'
 with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerow(data.keys())
